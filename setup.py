855d589b8cd8235ebd38f2ed10dd97eec017cd00
LukeZNotFound <luke.hent3005@gmail.com>
2024-05-18 04:59:00 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+BASE_URL = "https://github.com/The-LukeZ/"
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+
+setup(
+    name='TimestringPy',
+    version='1.0',
+    description='Parse a timestring into a timedelta object.',
+    long_description=README,
+    author='The-LukeZ',
+    author_email='luke.hent3005@gmail.com',
+    url=BASE_URL + "TimestringPy/",
+    py_modules=['timestring'],
+    scripts=['timestring.py'],
+    license='MIT'
+)
