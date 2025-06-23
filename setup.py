0b74fdd80acfaa303e9ade25d0304a75362390f5
The-LukeZ <luke.hent3005@gmail.com>
2025-06-23 03:04:20 +0200

    Refactor setup.py for improved readability and structure

--- setup.py
+++ setup.py
@@ -1,23 +1,28 @@
 import os
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup, find_packages
+import setuptools
 
 BASE_URL = "https://github.com/The-LukeZ/"
 
-here = os.path.abspath(os.path.dirname(__file__))
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-setup(
+
+setuptools.setup(
     name='TimestringPy',
-    version='1.1.2',
+    version='1.1.3',
     description='Parse a timestring into a floating number.',
-    long_description='file:README.md',
+    long_description=long_description,
     author='The-LukeZ',
     author_email='luke.hent3005@gmail.com',
     url=BASE_URL + "TimestringPy/",
-    packages=find_packages(),
+    packages=setuptools.find_packages(),
     license='MIT',
     long_description_content_type="text/markdown",
-    credits=["mike182uk"]
+    credits=["mike182uk"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
 )
